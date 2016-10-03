8b8197cc118093d839350bdfaee4a847bd10461d
Konstantin Kolomeetz <kolomeetz@gmail.com>
2016-10-03 10:31:27 +0300

    Add fields to user model

--- db/migrate/20160930104405_create_users.rb
+++ db/migrate/20160930104405_create_users.rb
@@ -1,6 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
+      t.integer :chat_id
+      t.string :first_name
+      t.string :last_name
+      t.string :username
 
       t.timestamps
     end
